2dbadb974c78b8616be515bf2a9d5e88348f0fdb
pello_altadill@cuatrovientos.org <pello_altadill@cuatrovientos.org@6db8b05b-7dd6-ce75-99fb-e0627217247c>
2013-08-29 21:42:03 +0000

    greetz 4 u

---
+++ AndroidPasswordService/src/info/pello/android/service/password/Logger.java
@@ -0,0 +1,37 @@
+package info.pello.android.service.password;
+
+import android.util.Log;
+
+/**
+ * Singleton class to log messages.
+ * @author Pello Xabier Altadill Izura
+ * @greetz my greetz as the constructor, are private
+ */
+public class Logger {
+	private static Logger logger = null;
+	private static long counter = 0; 
+	
+	/**
+	 * Logger private constructor
+	 */
+	private Logger () {}
+	
+	/**
+	 * getInstance
+	 * @return unique instance of Logger 
+	 */
+	public static Logger getInstance () {
+		if (null == logger) {
+			logger = new Logger();
+		} 	
+		return logger;
+	}
+	
+	/**
+	 * Logs messages using android's log facilities
+	 * @param msg
+	 */
+	public void log(String msg) {
+		Log.d("PassService ["+(counter++)+"]> " + msg,"PELLODEBUG");
+	}
+}
